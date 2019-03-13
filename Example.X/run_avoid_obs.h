21e8de4e377086dd5caaba651c905ed7d5f300ef
ggsggs <43816172+ggsggs@users.noreply.github.com>
2019-03-13 04:26:59 +0000

    header of avoid_obs GGS uploaded

---
+++ Example.X/run_avoid_obs.h
@@ -0,0 +1,9 @@
+#ifndef RUN_AVOID_OBS
+#define RUN_AVOID_OBS
+
+int setMinMax(int limit, int targetSpeed);
+void setWheelSpeeds(int side, int speed);
+void avoid_obst();
+void run_avoid_obs();
+
+#endif
